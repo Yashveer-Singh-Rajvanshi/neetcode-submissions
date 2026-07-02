a0588e4591e84b58f3c269094f2f1d86ca49e5a9
Yashveer Singh Rajvanshi <146558852+Yashveer-Singh-Rajvanshi@users.noreply.github.com>
2026-07-02 12:03:43 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hashmap = {}
+        for num in nums:
+            if num in hashmap:
+                hashmap[num] += 1
+            else:
+                hashmap[num] = 1
+
+        sorted_nums = sorted(hashmap,key=hashmap.get,reverse=True)
+
+        return sorted_nums[:k]
+
+        
