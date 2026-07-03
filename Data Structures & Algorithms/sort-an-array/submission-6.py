9386ad19c6064b7bfbfe3f15bc1ca33f41875bca
Yashveer Singh Rajvanshi <146558852+Yashveer-Singh-Rajvanshi@users.noreply.github.com>
2026-07-03 12:37:31 +0530

    Add: sort-an-array - submission-6

---
+++ Data Structures & Algorithms/sort-an-array/submission-6.py
@@ -0,0 +1,29 @@
+import random
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+
+        if len(nums) <= 1:
+            return nums
+
+        random_idx = random.randint(0,len(nums)-1)
+        nums[random_idx],nums[len(nums)-1] = nums[len(nums)-1],nums[random_idx]
+
+        pivot = len(nums) - 1
+        j = 0
+        i = j-1
+
+        while j < pivot:
+            if nums[j] <= nums[pivot]:
+                i+=1
+                nums[i],nums[j] = nums[j],nums[i]      
+            j += 1
+           
+        i+=1
+        nums[i],nums[pivot] = nums[pivot],nums[i]
+
+        left_sorted = self.sortArray(nums[:i])
+        right_sorted = self.sortArray(nums[i+1:])
+
+        return left_sorted + [nums[i]] + right_sorted
+        
