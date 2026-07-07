2295f7922c0e4011b7ad61c18925e710afdc0bb3
Yashveer Singh Rajvanshi <146558852+Yashveer-Singh-Rajvanshi@users.noreply.github.com>
2026-07-07 22:06:18 +0530

    Add: reverse-string - submission-1

---
+++ Data Structures & Algorithms/reverse-string/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        p1 = 0
+        p2 = len(s)-1
+
+        while p1<p2:
+            s[p1],s[p2] = s[p2],s[p1]
+            p1+=1
+            p2-=1
+        
